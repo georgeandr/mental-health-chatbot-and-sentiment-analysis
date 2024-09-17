0096597e9b58198a514c010ecb0dc45dc63c62ed
Georgios Andronikidis <giorgosandronikidis@hotmail.com>
2024-09-17 12:02:14 +0300

    add the functions for the endpoints

--- api/controllers/controllers.py
+++ api/controllers/controllers.py
@@ -1,16 +1,11 @@
-from fastapi import APIRouter
 from helpers.functions import preprocess_text
 from constants.constants import TextData
 import joblib
-
-router = APIRouter()
 
 # Load the saved model and vectorizer
 rf_model = joblib.load('../models/random_forest_model.pkl')
 vectorizer = joblib.load('../models/tfidf_vectorizer.pkl')
 
-# Prediction endpoint
-@router.post("/predict")
 def predict_sentiment(data: TextData):
     # Preprocess the input text
     input_processed = preprocess_text(data.cleaned_statement)
